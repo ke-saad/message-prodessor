f2f5bf13b14baf1d9071715f2becee85da58ceb8
ke-saad <kellalisaad@gmail.com>
2024-11-25 22:09:27 +0100

    commit

---
+++ src/test/java/com/example/message_processor/MessageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
